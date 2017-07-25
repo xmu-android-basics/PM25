7f7bf91ab9803454a0d1d9c6eb0a78f8ef6a3d03
windwood-xmu <windwood@xmu.edu.cn>
2017-07-25 23:16:45 +0800

    Starting point 代码模板，包含发起 HTTP 请求的并获取代码的基础代码 学生从这个基础开始实现按钮点击的响应事件

---
+++ app/src/main/java/io/github/xmu_android_basics/pm25/MainActivity.java
@@ -0,0 +1,133 @@
+package io.github.xmu_android_basics.pm25;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
+
+public class MainActivity extends AppCompatActivity {
+
+    /** Tag for the log messages */
+    public static final String LOG_TAG = MainActivity.class.getSimpleName();
+
+    /** URL to query the cities of PM2.5 dataset */
+    private static final String PM25_CITIES_REQUEST_URL =
+            "http://www.pm25.in/api/querys.json?token=5j1znBVAsnSf5xQyNQyq";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onButtonClick(View view) {
+    }
+
+    /**
+     * Update the screen to display information from the given response.
+     */
+    private void updateUi(String response) {
+        TextView responseTextView = (TextView) findViewById(R.id.response);
+
+        responseTextView.setText(response);
+    }
+
+    private String queryData(String queryString) {
+        // Create URL object
+        URL url = createUrl(queryString);
+
+        // Perform HTTP request to the URL and receive a JSON response back
+        String response = "";
+        try {
+            response = makeHttpRequest(url);
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Problem making the HTTP request.", e);
+        }
+
+        return response;
+    }
+
+    /**
+     * Returns new URL object from the given string URL.
+     */
+    private URL createUrl(String stringUrl) {
+        URL url = null;
+        try {
+            url = new URL(stringUrl);
+        } catch (MalformedURLException exception) {
+            Log.e(LOG_TAG, "Error with creating URL", exception);
+            return null;
+        }
+        return url;
+    }
+
+    /**
+     * Make an HTTP request to the given URL and return a String as the response.
+     */
+    private String makeHttpRequest(URL url) throws IOException {
+        String response = "";
+
+        // If the URL is null, then return early.
+        if (url == null) {
+            return response;
+        }
+
+        HttpURLConnection urlConnection = null;
+        InputStream inputStream = null;
+        try {
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.setReadTimeout(10000 /* milliseconds */);
+            urlConnection.setConnectTimeout(15000 /* milliseconds */);
+            urlConnection.connect();
+
+            // If the request was successful (response code 200),
+            // then read the input stream and parse the response.
+            if (urlConnection.getResponseCode() == 200) {
+                inputStream = urlConnection.getInputStream();
+                response = readFromStream(inputStream);
+            } else {
+                Log.e(LOG_TAG, "Error response code: " + urlConnection.getResponseCode());
+            }
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Problem retrieving the earthquake JSON results.", e);
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (inputStream != null) {
+                // function must handle java.io.IOException here
+                inputStream.close();
+            }
+        }
+        return response;
+    }
+
+    /**
+     * Convert the {@link InputStream} into a String which contains the
+     * whole JSON response from the server.
+     */
+    private String readFromStream(InputStream inputStream) throws IOException {
+        StringBuilder output = new StringBuilder();
+        if (inputStream != null) {
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
+            BufferedReader reader = new BufferedReader(inputStreamReader);
+            String line = reader.readLine();
+            while (line != null) {
+                output.append(line);
+                line = reader.readLine();
+            }
+        }
+        return output.toString();
+    }
+}
